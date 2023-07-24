0878ba636d3226b6984a258556bc8d73e6d73d61
Denislive <dennygachemi6@gmail.com>
2023-07-24 12:01:44 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -3,17 +3,85 @@
 #include <stdarg.h>
 
 /**
+ * print_string - Print a string.
+ * @args: The va_list that contains the string to print.
+ *
+ * Return: The number of characters printed.
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+
+	int printed_chars = 0;
+	char current_char;
+
+	while ((current_char = *str++))
+	{
+		_putchar(current_char);
+		printed_chars++;
+	}
+	return (printed_chars);
+}
+
+/**
+ * print_integer - Print an integer.
+ * @args: The va_list that contains the integer to print.
+ *
+ * Return: The number of characters printed.
+ */
+int print_integer(va_list args)
+{
+	int num = va_arg(args, int);
+	int temp = num;
+	int digits = 0;
+
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	else if (num < 0)
+	{
+		_putchar('-');
+		num *= -1;
+		temp *= -1;
+		digits++;
+	}
+
+	int divisor = 1;
+	int j;
+
+	for (j = 1; j < digits; j++)
+		divisor *= 10;
+
+	int printed_chars = 0;
+
+	while (divisor != 0)
+	{
+		_putchar((num / divisor) + '0');
+		printed_chars++;
+		num %= divisor;
+		divisor /= 10;
+	}
+
+	return (printed_chars);
+}
+
+/**
  * _printf - Produces output according to a format.
  * @format: The character string containing zero or more directives.
+ * @...: Optional arguments to be printed according to the format.
  *
- * Return: The number of characters printed (excluding the null byte).
+ * Return: The number of characters printed (excluding the null byte),
+ * or -1 if the format is NULL.
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int printed_chars = 0;
-	char *str;
-	int num, temp, digits, divisor, j;
 
 	va_start(args, format);
 
@@ -28,56 +96,14 @@ int _printf(const char *format, ...)
 			switch (*format)
 			{
 				case 'c':
-					_putchar(va_arg(args, int));
-					printed_chars++;
+					printed_chars += print_char(args);
 					break;
 				case 's':
-					str = va_arg(args, char *);
-					if (str == NULL)
-						str = "(null)";
-					while (*str)
-					{
-						_putchar(*str);
-						printed_chars++;
-						str++;
-					}
+					printed_chars += print_string(args);
 					break;
 				case 'd':
 				case 'i':
-					num = va_arg(args, int);
-					temp = num;
-					digits = 0;
-
-					if (num == 0)
-					{
-						_putchar('0');
-						printed_chars++;
-					}
-					else if (num < 0)
-					{
-						_putchar('-');
-						printed_chars++;
-						num *= -1;
-						temp *= -1;
-					}
-
-					while (temp != 0)
-					{
-						temp /= 10;
-						digits++;
-					}
-
-					divisor = 1;
-					for (j = 1; j < digits; j++)
-						divisor *= 10;
-
-					while (divisor != 0)
-					{
-						_putchar((num / divisor) + '0');
-						printed_chars++;
-						num %= divisor;
-						divisor /= 10;
-					}
+					printed_chars += print_integer(args);
 					break;
 				case '%':
 					_putchar('%');
